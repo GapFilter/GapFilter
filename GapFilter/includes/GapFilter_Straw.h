5bee07bd02ce33c421f036733a62e323d6b33509
GapFilter <139448653+GapFilter@users.noreply.github.com>
2023-11-03 15:23:13 +0800

    Add files via upload

--- GapFilter/includes/GapFilter_Straw.h
+++ GapFilter/includes/GapFilter_Straw.h
@@ -1,6 +1,7 @@
 #ifndef _GAPFILTER_STRAW_H
 #define _GAPFILTER_STRAW_H
 
+#include <x86intrin.h>
 #include <bits/stdc++.h>
 #include "BOBHash64.h"
 #include "params.h"
@@ -11,12 +12,13 @@ using namespace std;
 class GapFilter_Straw : public GapFilter
 {
 public:
-    struct cell{
-        uint64_t fingerprint;
-        Seq seq;};
-    typedef cell* bucket;
-    const int bucket_size=4, fp_length=32;
-    const int MAXTURN=8;
+    const __m64 m64zero = {0};
+    const __m128i m128zero = {0, 0};
+    struct bucket{
+    __m128i m128_fp;
+    __m64 m64_seq;
+    };
+    const int MAXTURN = 8;
     uint64_t bucket_num, blockbound;
     bucket *table;
     BOBHash64 *hash_index1, *hash_index2, *hash_fp;
@@ -25,102 +27,108 @@ public:
     GapFilter_Straw(int memory, int hash_seed = 1000)
         : GapFilter(memory)
     {
-        bucket_num = (memory / (fp_length/8 + sizeof(Seq))) / bucket_size;
+        bucket_num = (memory / (sizeof(uint32_t) + sizeof(Seq))) / 4;
         blockbound = bucket_num / 2;
         table = new bucket[bucket_num];
         for(uint64_t i = 0; i < bucket_num; i++){
-            table[i] = new cell[bucket_size];
-            memset(table[i], 0, sizeof(cell) * bucket_size);
+            memset(&table[i], 0, sizeof(bucket));
         }
 
         srand(hash_seed);
         hash_index1 = new BOBHash64(rand()), hash_index2 = new BOBHash64(rand());
         hash_fp = new BOBHash64(rand());
     }
-
+ 
     bool Solution(const Packet &packet)
     {
         bool res = false; 
-        uint64_t fp = (hash_fp->run((const char *)packet.id.data, FLOWID_LEN)) % (0x1ull << fp_length);
-        uint64_t index_1 = (hash_index1->run((const char *)&fp, sizeof(uint64_t))) % blockbound;
-        uint64_t index_2 = (hash_index2->run((const char *)&fp, sizeof(uint64_t))) % blockbound + blockbound;
+        uint32_t fp = (hash_fp->run((const char *)packet.id.data, FLOWID_LEN)) % (0x100000000ull);
+        uint64_t index_1 = (hash_index1->run((const char *)&fp, sizeof(uint32_t))) % blockbound;
+        uint64_t index_2 = (hash_index2->run((const char *)&fp, sizeof(uint32_t))) % blockbound + blockbound;
+        bucket *b1 = &table[index_1], *b2 = &table[index_2];
         
-        bucket b1 = table[index_1], b2 = table[index_2];
-        cell *match_cell = NULL, *empty_cell = NULL;
-
-        for(int i = 0; i < bucket_size; i++){
-            if(b1[i].fingerprint == fp){
-                match_cell = &b1[i];
-                goto find;
-            }
-            if(empty_cell == NULL && b1[i].fingerprint == 0 && b1[i].seq == 0){
-                empty_cell = &b1[i];
-            }
-        }
-        for(int i = 0; i < bucket_size; i++){
-            if(b2[i].fingerprint == fp){
-                match_cell = &b2[i];
-                goto find;
-            }
-            if(empty_cell == NULL && b2[i].fingerprint == 0 && b2[i].seq == 0){
-                empty_cell = &b2[i];
-            }
+        __m128i m128i_visitfp = (__m128i)_mm_set1_epi32(fp); 
+        __m128i m128i_eqres_1, m128i_eqres_2;
+        int tailzero_1, tailzero_2, matchplace_1, matchplace_2;
+        m128i_eqres_1 = _mm_cmpeq_epi32(m128i_visitfp, b1->m128_fp);
+        tailzero_1 = __tzcnt_u32(_mm_movemask_epi8(m128i_eqres_1));
+        matchplace_1 = tailzero_1 / 4;
+        if(matchplace_1 < 4){
+            uint16_t *match_cell_seq = &(((uint16_t*)&b1->m64_seq)[matchplace_1]);
+            uint16_t delta = packet.seq - *match_cell_seq;
+            if (delta >= T1 && delta < T2)
+                res = true;
+            if(packet.seq > *match_cell_seq || *match_cell_seq - packet.seq >= THRES)
+                *match_cell_seq = packet.seq;            
+            return res;
         }
 
-        if(empty_cell != NULL){
-            empty_cell->fingerprint = fp;
-            empty_cell->fingerprint = packet.seq;
+        m128i_eqres_2 = _mm_cmpeq_epi32(m128i_visitfp, b2->m128_fp);
+        tailzero_2 = __tzcnt_u32(_mm_movemask_epi8(m128i_eqres_2));
+        matchplace_2 = tailzero_2 / 4;
+        if(matchplace_2 < 4){
+            uint16_t *match_cell_seq = &(((uint16_t*)&b2->m64_seq)[matchplace_2]);
+            uint16_t delta = packet.seq - *match_cell_seq;
+            if (delta >= T1 && delta < T2)
+                res = true;
+            if(packet.seq > *match_cell_seq || *match_cell_seq - packet.seq >= THRES)
+                *match_cell_seq = packet.seq;            
+            return res;
         }
-        else{
-            uint64_t kick_index = index_1;
-            if(rand()%2==0) kick_index = index_2;
 
-            uint64_t insert_fp = fp;
-            Seq insert_seq = packet.seq;
-            for(int turn = 0; turn < MAXTURN; turn++){
-                int kick_i = rand() % bucket_size;
-                cell* kick_cell = &table[kick_index][kick_i];
-                uint64_t kick_fp = kick_cell->fingerprint;
-                Seq kick_seq = kick_cell->seq;
-                kick_cell->fingerprint = insert_fp;
-                kick_cell->seq = insert_seq;
+        m128i_eqres_1 = _mm_cmpeq_epi32(m128zero, b1->m128_fp);
+        tailzero_1 = __tzcnt_u32(_mm_movemask_epi8(m128i_eqres_1));
+        matchplace_1 = tailzero_1 / 4;
+        if(matchplace_1 < 4){
+            ((uint32_t*)&b1->m128_fp)[matchplace_1] = fp;
+            ((uint16_t*)&b1->m64_seq)[matchplace_1] = packet.seq;
+            return res;
+        }
 
-                if(kick_index < blockbound)
-                    kick_index = ((hash_index2->run((const char *)&kick_fp, sizeof(uint64_t)))) % blockbound + blockbound;
-                else
-                    kick_index = ((hash_index1->run((const char *)&kick_fp, sizeof(uint64_t)))) % blockbound;
-                for(int i = 0; i < bucket_size; i++){
-                    if(table[kick_index][i].fingerprint == 0 && table[kick_index][i].seq == 0){
-                        table[kick_index][i].fingerprint = kick_fp;
-                        table[kick_index][i].seq = kick_seq;
-                        return res;
-                    }
-                }
-                insert_fp = kick_fp;
-                insert_seq = kick_seq;
+        m128i_eqres_2 = _mm_cmpeq_epi32(m128zero, b2->m128_fp);
+        tailzero_2 = __tzcnt_u32(_mm_movemask_epi8(m128i_eqres_2));
+        matchplace_2 = tailzero_2 / 4;
+        if(matchplace_2 < 4){
+            ((uint32_t*)&b2->m128_fp)[matchplace_2] = fp;
+            ((uint16_t*)&b2->m64_seq)[matchplace_2] = packet.seq;
+            return res;
+        }
+
+        uint64_t kick_index = index_1;
+        if(rand()%2==0) kick_index = index_2;
+        uint32_t insert_fp = fp;
+        uint16_t insert_seq = packet.seq;
+        for(int turn = 0; turn < MAXTURN; turn++){
+            int kick_i = rand() % 4;
+            uint32_t kick_fp = ((uint32_t*)&table[kick_index].m128_fp)[kick_i];
+            uint16_t kick_seq = ((uint16_t*)&table[kick_index].m64_seq)[kick_i];
+            ((uint32_t*)&table[kick_index].m128_fp)[kick_i] = insert_fp;
+            ((uint16_t*)&table[kick_index].m64_seq)[kick_i] = insert_seq;
+
+            if(kick_index < blockbound)
+                kick_index = ((hash_index2->run((const char *)&kick_fp, sizeof(uint32_t)))) % blockbound + blockbound;
+            else
+                kick_index = ((hash_index1->run((const char *)&kick_fp, sizeof(uint32_t)))) % blockbound;
+            __m128i m128_eqres = _mm_cmpeq_epi32(m128zero, table[kick_index].m128_fp);
+            int tailzero = __tzcnt_u32(_mm_movemask_epi8(m128_eqres));
+            int matchplace = tailzero / 4;
+            if(matchplace < 4){
+                ((uint32_t*)&table[kick_index].m128_fp)[matchplace] = kick_fp;
+                ((uint16_t*)&table[kick_index].m64_seq)[matchplace] = kick_seq;
+                return res;
             }
+            insert_fp = kick_fp;
+            insert_seq = kick_seq;
         }
-        return res;
-
-find:
-        uint16_t delta = packet.seq - match_cell->seq;
-        if (delta >= T1 && delta < T2)
-            res = true;
-        if(packet.seq > match_cell->seq || match_cell->seq - packet.seq >= THRES)
-            match_cell->seq = packet.seq;
-
         return res;
     }
- 
+
     string Name()
     {
         return string("GapFilter_Straw");
     }
-
     ~GapFilter_Straw()
     {
-        for(uint64_t i = 0; i < bucket_num; i++)
-            delete[] table[i];
         delete[] table;
         delete hash_index1, hash_index2;
         delete hash_fp;
