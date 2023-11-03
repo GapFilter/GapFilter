e965c6577c72d565738759516999403c3f42241d
GapFilter <139448653+GapFilter@users.noreply.github.com>
2023-11-03 14:10:30 +0800

    Add files via upload

--- GapFilter/includes/wosimd.h
+++ GapFilter/includes/wosimd.h
@@ -2,6 +2,7 @@
 #define _WOSIMD_H
 
 #include <bits/stdc++.h>
+#include "params.h"
 using namespace std;
 
 class SO_woSIMD_Bucket_4_16     
@@ -54,88 +55,6 @@ public:
             num[0] = visitnum;
             return 2;
         }
-    }
-};
-
-
-class woSIMD_Bucket_8_16_fpn
-{
-public: 
-    uint16_t sz;
-    uint16_t num_fp[8];
-    uint16_t num_seq[8];
-
-    woSIMD_Bucket_8_16_fpn(uint16_t sz_)
-    {
-        sz = sz_;
-        memset(num_fp, 0, sizeof(num_fp));
-        memset(num_seq, 0, sizeof(num_seq));
-    }
-
-    int Insert(uint16_t seq, uint16_t fp)
-    {
-        int matchplace = -1;
-        int32_t min_diff = 0x7ffffff;
-        for(int i = 0; i < 8; i++){
-            if(num_fp[i] == fp){
-                int16_t delta = (int16_t)seq - (int16_t)num_seq[i];
-                if(abs(delta) < abs(min_diff)){
-                    min_diff = delta;
-                    matchplace = i;
-                }
-            }
-        }
-        
-        if(int16_t(-30) < min_diff && min_diff < 30){
-            if(min_diff < 5){
-                if(min_diff > 0)
-                    num_seq[matchplace] = seq;
-                if(matchplace >= sz){
-                    uint16_t tmp_fp = num_fp[matchplace], 
-                            tmp_seq = num_seq[matchplace];
-                    for(int i = matchplace; i > sz; i--){
-                        num_fp[i] = num_fp[i-1];
-                        num_seq[i] = num_seq[i-1];
-                    }
-                    num_fp[sz] = tmp_fp;
-                    num_seq[sz] = tmp_seq;
-                }
-                return 0;
-            }
-            else{
-                for(int i = 7; i > 0; i--){
-                    num_fp[i] = num_fp[i-1];
-                    num_seq[i] = num_seq[i-1];
-                }
-                num_fp[0] = fp;
-                num_seq[0] = seq;
-                return 1;
-            }
-        }
-        else{
-            int emptyplace = 7; 
-            for(int i = 0; i < 8; i++){
-                if(num_fp[i] == 0 && num_seq[i] == 0){
-                    emptyplace = i;
-                    break;
-                }
-            }
-
-            if(emptyplace < sz){
-                num_fp[emptyplace] = fp;
-                num_seq[emptyplace] = seq;
-            }
-            else{
-                for(int i = emptyplace; i > sz; i--){
-                    num_fp[i] = num_fp[i-1];
-                    num_seq[i] = num_seq[i-1];
-                }
-                num_fp[sz] = fp;
-                num_seq[sz] = seq;
-            }
-            return 2;
-        }
-        return 2;
     }
 };
 
@@ -235,6 +154,88 @@ public:
     }
 };
 
+
+class woSIMD_Bucket_8_16_fpn
+{
+public: 
+    uint16_t sz;
+    uint16_t num_fp[8];
+    uint16_t num_seq[8];
+
+    woSIMD_Bucket_8_16_fpn(uint16_t sz_)
+    {
+        sz = sz_;
+        memset(num_fp, 0, sizeof(num_fp));
+        memset(num_seq, 0, sizeof(num_seq));
+    }
+
+    int Insert(uint16_t seq, uint16_t fp)
+    {
+        int matchplace = -1;
+        int32_t min_diff = 0x7ffffff;
+        for(int i = 0; i < 8; i++){
+            if(num_fp[i] == fp){
+                int16_t delta = (int16_t)seq - (int16_t)num_seq[i];
+                if(abs(delta) < abs(min_diff)){
+                    min_diff = delta;
+                    matchplace = i;
+                }
+            }
+        }
+        
+        if(-30 < min_diff && min_diff < 30){
+            if(min_diff < 5){
+                if(min_diff > 0)
+                    num_seq[matchplace] = seq;
+                if(matchplace > sz){
+                    uint16_t tmp_fp = num_fp[matchplace], 
+                            tmp_seq = num_seq[matchplace];
+                    for(int i = matchplace; i > sz; i--){
+                        num_fp[i] = num_fp[i-1];
+                        num_seq[i] = num_seq[i-1];
+                    }
+                    num_fp[sz] = tmp_fp;
+                    num_seq[sz] = tmp_seq;
+                }
+                return 0;
+            }
+            else{
+                for(int i = 7; i > 0; i--){
+                    num_fp[i] = num_fp[i-1];
+                    num_seq[i] = num_seq[i-1];
+                }
+                num_fp[0] = fp;
+                num_seq[0] = seq;
+                return 1;
+            }
+        }
+        else{
+            int emptyplace = 7; 
+            for(int i = 0; i < 8; i++){
+                if(num_fp[i] == 0 && num_seq[i] == 0){
+                    emptyplace = i;
+                    break;
+                }
+            }
+
+            if(emptyplace < sz){
+                num_fp[emptyplace] = fp;
+                num_seq[emptyplace] = seq;
+            }
+            else{
+                for(int i = emptyplace; i > sz; i--){
+                    num_fp[i] = num_fp[i-1];
+                    num_seq[i] = num_seq[i-1];
+                }
+                num_fp[sz] = fp;
+                num_seq[sz] = seq;
+            }
+            return 2;
+        }
+        return 2;
+    }
+};
+
 class woSIMD_Bucket_16_16_fpn
 {
 public: 
@@ -263,11 +264,11 @@ public:
             }
         }
         
-        if(int16_t(-30) < min_diff && min_diff < 30){
+        if(-30 < min_diff && min_diff < 30){
             if(min_diff < 5){
                 if(min_diff > 0)
                     num_seq[matchplace] = seq;
-                if(matchplace >= sz){
+                if(matchplace > sz){
                     uint16_t tmp_fp = num_fp[matchplace], 
                             tmp_seq = num_seq[matchplace];
                     for(int i = matchplace; i > sz; i--){
